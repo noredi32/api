d12fd7531d950d592fe24afe3664aa001842397c
Norma Badillo <noredi@gmail.com>
2022-07-12 16:39:31 -0500

    No

--- app.js
+++ app.js
@@ -18,18 +18,3 @@ app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
     console.log("Server is running.");
   });
 
-var conexion = mysql.createConnection({
-    host: 'us-cdbr-east-06.cleardb.net',
-    user: 'b62d42f0eb10ff',
-    password: 'beca043a',
-    database: 'heroku_0934279acadb2ff',
-    port: '3000',
-});
-
-conexion.connect(function(error) {
-    if (error) {
-        throw error;
-    } else {
-        console.log('Conexión exitosa');
-    }
-})
