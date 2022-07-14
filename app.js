07056973ad8c47a031a10be5b1969b49ee82dd73
Norma Badillo <noredi@gmail.com>
2022-07-14 01:59:08 -0500

    1.59

--- app.js
+++ app.js
@@ -10,8 +10,18 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', require('./routes'));
-app.listen(3001, () => {
+/*app.listen(3001, () => {
     console.log(`Express on port 3001`);
 });
+*/
 
+app.set('port', (processs.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request,response){
+    var result = 'App is running'
+    response.send(result);
+}) .listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port', app.get('port'));
+});
 
