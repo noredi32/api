930c88d31f8aa94f4caeac85223bd22ac4858d53
Norma Badillo <noredi@gmail.com>
2022-07-14 02:16:58 -0500

    2.16

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ app.use('/api', require('./routes'));
 });
 */
 
-app.set('port', (processs.env.PORT || 5000));
+app.set('PORT', (processs.env.PORT || 3000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request,response){
     var result = 'App is running'
     response.send(result);
-}) .listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port', app.get('port'));
+}) .listen(app.get('PORT'), function() {
+    console.log('App is running, server is listening on port', app.get('PORT'));
 });
 
